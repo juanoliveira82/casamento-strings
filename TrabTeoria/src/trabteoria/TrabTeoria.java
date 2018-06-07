187a433ba5e0cba4970542163a9768bae2bd2c3c
Juan Carlos Cardoso de Oliveira <juanplaystation82@gmail.com>
2018-06-06 23:28:01 -0300

    inicio

---
+++ TrabTeoria/src/trabteoria/TrabTeoria.java
@@ -0,0 +1,27 @@
+package trabteoria;
+
+/**
+ *
+ * @author Juan
+ */
+public class TrabTeoria {
+
+    void comecaCom(String palavra, String verificacao){
+        // palavra = banana
+        // verificação = verificar
+        palavra;
+    }
+    
+    void terminaCom(String palavra, String verificacao){
+        
+    }
+    
+    void contem(String palavra, String verificacao){
+        
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
